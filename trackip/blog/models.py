732126e5b524ef72e528d86f34fa07b4a7146b08
python-hacked <satishchoudhary394@gmail.com>
2023-04-09 00:43:33 +0530

    add requirements

--- trackip/blog/models.py
+++ trackip/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Post(models.Model):
- title = models.CharField(max_length=150)
- desc = models.TextField()
+    title = models.CharField(max_length=150)
+    desc = models.TextField()
